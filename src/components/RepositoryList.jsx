9f9a82cabbd14bfdee6239ff537f9171a180666f
Luiz Felipe <lfsprj@gmail.com>
2021-09-28 13:03:44 -0300

    Added main files

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from "react"
+import { RepositoryItem } from "./RepositoryItem"
+import '../styles/repositories.scss'
+
+
+
+
+
+
+
+export function RepositoryList() {
+
+    const [repositories, setRepositories] = useState([]);
+
+    useEffect(() => { 
+         fetch('https://api.github.com/users/Luiz-Fel/repos', {
+            headers: {
+                'Authorization': 'token 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',
+            },
+        })
+        .then(response => response.json())
+        .then(data => setRepositories(data))
+    });
+
+
+    return (
+        
+
+        <section className="repository-list">
+        <h1>Lista de repositórios</h1>
+        <ul>
+            {repositories.map(repository => {
+                return <RepositoryItem repository={repository} />
+            })}
+             
+        </ul>
+        </section>
+    );
+}
---
+++ src/index.jsx
@@ -0,0 +1,6 @@
+import './styles/global.scss';
+import { render } from "react-dom";
+import { App } from './App'
+
+render(<App />, document.getElementById('root'))
+
